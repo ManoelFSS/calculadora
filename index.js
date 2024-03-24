e94642acd3438015a8f1c83e342f2745e2a71384
Manoel <piloukos45@gmail.com>
2024-03-23 23:22:40 -0300

    teste

--- index.js
+++ index.js
@@ -8,21 +8,39 @@ const clear = document.querySelector(".clear")
 const esc = document.querySelector(".esc")
 
 let type = false;
+let cursor = false;
+
 
 btn.forEach((btn)=>{
     btn.addEventListener("click", (e)=>{
         const display = document.querySelector(".display .formula")
-        display.innerText += e.target.innerText
-        type = false
+        
+        if(display.innerText === "|"){
+            display.innerText = e.target.innerText
+            display.classList.remove("ativo")
+           
+        }else{
+            display.innerText += e.target.innerText
+            type = false
+        }
+
+        
     })
 })
+
 
 opera.forEach((opera)=>{
     opera.addEventListener("click", (e)=>{
         const display = document.querySelector(".display .formula")
-        console.log(display.innerText)
+        display.classList.remove("ativo")
+
+        if(display.innerText === "|" ){
+           display.innerText =  "0" 
+        }
+
         if(!type){
-            display.innerText += e.target.innerText === "." ? "0." : display.innerText === "" ? "0" + e.target.innerText : e.target.innerText
+           
+            display.innerText += display.innerText === "." ? "0." + e.target.innerText : e.target.innerText
             type = true
         }
     })
@@ -33,19 +51,30 @@ acao.addEventListener("click", () => {
     const result = document.querySelector(".display .formula");
     const res = document.querySelector(".display .resultado");
     const n = eval(result.innerText);
-    res.innerText = n
-    
+    res.innerText = n  
 });
 
 clear.addEventListener("click", ()=>{
     const result = document.querySelector(".display .formula");
+    const total = document.querySelector(".display .resultado");
     const res = result.innerText.split("")
     res.pop()
     result.innerText = res.join("")
+
+    if(res.length == 0){
+        total.innerHTML = "0"
+        result.innerText = "|"
+        result.classList.add("ativo")
+        cursor = false
+    }
 })
 
 esc.addEventListener("click", ()=>{
     const result = document.querySelector(".display .formula");
-    result.innerText = ""
+    const res = document.querySelector(".display .resultado");
+    result.innerText = "|"
+    res.innerText = ""
+    result.classList.add("ativo")
+    cursor = false
 })
 
